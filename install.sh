315366faab906623b36953ab037aa94fd478fa71
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2021-05-09 20:43:52 -0700

    Update linux install.sh
    This updates the install file for wifi capabilities and fix some install issues.

--- install.sh
+++ install.sh
@@ -9,19 +9,36 @@ fi
 
 echo "Root check passed, continue install."
 
-apt-get install build-essential checkinstall git autoconf automake
+apt install git -y
 
-apt install libtool m4 automake
+apt-get upgrade -y
+
+apt-get install build-essential checkinstall git autoconf automake -y
+
+apt install libtool m4 automake -y
+
+apt-get install python-dev -y
+
+apt-get install pkg-config -y
+
+apt-get install libavahi-client-dev -y
+
+
+
+apt-get install libusb-1.0-0-dev
+
+apt-get install libssl-dev
+
 
 git clone https://github.com/libimobiledevice/libplist
 cd libplist
-apt-get install python-dev --assume-yes
-apt-get install pkg-config --assume-yes
 ./autogen.sh
 ./autogen.sh
 make
 make install
 cd ../
+
+
 git clone https://github.com/jkcoxson/libusbmuxd
 cd libusbmuxd
 ./autogen.sh
@@ -29,6 +46,7 @@ cd libusbmuxd
 make
 make install
 cd ../
+
 git clone https://github.com/jkcoxson/libimobiledevice
 cd libimobiledevice
 ./autogen.sh
@@ -36,12 +54,32 @@ cd libimobiledevice
 make
 make install
 cd ../
+
+git clone https://github.com/tihmstar/usbmuxd2.git
+cd usbmuxd2
+./autogen.sh
+./autogen.sh
+make
+make install
+cd ../
+
+git clone https://github.com/tihmstar/libgeneral.git
+cd libgeneral
+./autogen.sh
+./autogen.sh
+make
+make install
+cd ../
+
+apt-get install libplist++ -y
+
+
 ldconfig
 echo
 echo
 echo
 echo "##############################"
-echo "# Script has been installed. #"
+echo "#   Installation Completed.  #"
 echo "##############################\n\n\n"
 
 idevice_id
