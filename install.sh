be02bf3e71c80f22a1938c3112306e8ec17f3cdd
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2021-06-11 15:56:58 -0700

    Linux fix for rasbian debian buster

--- install.sh
+++ install.sh
@@ -9,31 +9,69 @@ fi
 
 echo "Root check passed, continue install."
 
-apt install git -y
+apt-get install git -y
 
+apt install build-essential -y
+
+apt-get install glibtool -y
+apt-get install make -y
+
+
+apt-get install cpp -y
+apt-get install gcc-8 -y
+
+
+apt-get update -y
 apt-get upgrade -y
 
+
+apt-get clang -y
 apt-get install build-essential checkinstall git autoconf automake -y
 
 apt install libtool m4 automake -y
 
 apt-get install python-dev -y
-
 apt-get install pkg-config -y
 
 apt-get install libavahi-client-dev -y
+apt-get install python-dev --fix-missing -y
 
 
+apt-get install cython -y
+apt-get install cython --install-option="--no-cython-compile" -y
 
+
+apt-get autoheader -y
 apt-get install libusb-1.0-0-dev -y
 
-apt-get install libssl-dev -y
 
+apt-get install libssl-dev -y
+apt-get install libc6-udeb -y
+
+
+apt-get install libc6-dev -y
+apt-get install libtool-bin -y
+
+
+apt-get install libplist++-dev -y
+apt-get install libplist++-dev -y
+
+
+mkdir openssl
+cd openssl
+wget https://www.openssl.org/source/openssl-${OPENSSL_VER}.tar.gz
+tar xf openssl-${OPENSSL_VER}.tar.gz
+cd openssl-${OPENSSL_VER}
+./config zlib shared no-ss13
+make -j4
+make install
+cd ..
+cd ..
 
 git clone https://github.com/libimobiledevice/libplist
 cd libplist
-./autogen.sh
-./autogen.sh
+./autogen.sh --enable-debug
+./autogen.sh --enable-debug
 make
 make install
 cd ../
@@ -41,36 +79,37 @@ cd ../
 
 git clone https://github.com/jkcoxson/libusbmuxd
 cd libusbmuxd
-./autogen.sh
-./autogen.sh
+./autogen.sh --enable-debug
+./autogen.sh --enable-debug
 make
 make install
 cd ../
 
 git clone https://github.com/jkcoxson/libimobiledevice
 cd libimobiledevice
-./autogen.sh
-./autogen.sh
+./autogen.sh --enable-debug
+./autogen.sh --enable-debug
 make
 make install
 cd ../
 
 git clone https://github.com/jkcoxson/usbmuxd2
 cd usbmuxd2
-./autogen.sh
-./autogen.sh
+./autogen.sh --enable-debug
+./autogen.sh --enable-debug
 make
 make install
 cd ../
 
 git clone https://github.com/tihmstar/libgeneral.git
 cd libgeneral
-./autogen.sh
-./autogen.sh
+./autogen.sh --enable-debug
+./autogen.sh --enable-debug
 make
 make install
 cd ../
 
+apt-get install libplist++-dev -y
 apt-get install libplist++ -y
 
 
