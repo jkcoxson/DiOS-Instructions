d03d3348e3b378d96da21c178f0e9f1380bf1386
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2021-09-28 00:51:46 -0700

    Adds Semi-Automated SSHKEY setuo=p

---
+++ Windows_files_workaround/SshKeySetup/PS1scripts/SetupSSH.ps1
@@ -0,0 +1,17 @@
+﻿Start-Process powershell -verb runas -ArgumentList "-file fullpathofthescript"
+Set-Service sshd -StartupType Automatic
+Start-Service sshd
+
+New-NetFirewallRule -Name sshd -DisplayName 'Allow SSH' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+
+md ~/.ssh
+cd ~/.ssh
+ssh-keygen
+remove-item ~/.ssh/authorized_keys.pub
+echo .
+echo .
+echo "you will be directed to authorzed keys section..."
+echo .
+pause
+explorer.exe .
+explorer.exe C:\ProgramData\ssh
